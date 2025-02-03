5bdfc96729ff9e0d164a53b5026912323e1ac00c
arpitbhau <arpitnikhade20@gmail.com>
2025-02-03 07:11:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Set target date to December 31, 2025 23:59:59
+var targetDate = new Date("December 31, 2025 23:59:59");
+var now = new Date();
+
+// Calculate the difference in seconds
+var diff = (targetDate.getTime() - now.getTime()) / 1000;
+
+var clock = $('#clock1').FlipClock(diff, {
+    clockFace: 'DailyCounter', // Changed to DailyCounter to show days
+    countdown: true,
+    showSeconds: true
+});
